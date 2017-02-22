37639032aabc6dac747d4827c1a411e4cf738b64
Trevor Laughlin <trevor@laughlinresearch.com>
2017-02-22 11:34:18 -0500

    Create mesh at beginnin of OCCGenerateMesh

--- libsrc/occ/occgenmesh.cpp
+++ libsrc/occ/occgenmesh.cpp
@@ -1280,7 +1280,7 @@ namespace netgen
       if (mparam.perfstepsstart <= MESHCONST_ANALYSE)
       {
         // delete mesh;
-        // mesh = make_shared<Mesh>();
+         mesh = make_shared<Mesh>();
          mesh->geomtype = Mesh::GEOM_OCC;
          
          OCCSetLocalMeshSize(geom,*mesh);
