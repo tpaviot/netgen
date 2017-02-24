e31eaaadec433834c9726eee0f0c61cc8b063c4e
Trevor Laughlin <trevor@laughlinresearch.com>
2017-02-23 23:39:07 -0500

    netgen53 patch for occgeom.hpp

--- libsrc/occ/occgeom.hpp
+++ libsrc/occ/occgeom.hpp
@@ -15,8 +15,8 @@
 #include "Geom_Curve.hxx"
 #include "Geom2d_Curve.hxx"
 #include "Geom_Surface.hxx"
-#include "GeomAPI_ProjectPointOnSurf.hxx"
-#include "GeomAPI_ProjectPointOnCurve.hxx"
+//#include "GeomAPI_ProjectPointOnSurf.hxx"
+//#include "GeomAPI_ProjectPointOnCurve.hxx"
 #include "BRepTools.hxx"
 #include "TopExp.hxx"
 #include "BRepBuilderAPI_MakeVertex.hxx"
@@ -42,8 +42,8 @@
 #include "Geom_Curve.hxx"
 #include "Geom2d_Curve.hxx"
 #include "Geom_Surface.hxx"
-#include "GeomAPI_ProjectPointOnSurf.hxx"
-#include "GeomAPI_ProjectPointOnCurve.hxx"
+//#include "GeomAPI_ProjectPointOnSurf.hxx"
+//#include "GeomAPI_ProjectPointOnCurve.hxx"
 #include "TopoDS_Wire.hxx"
 #include "BRepTools_WireExplorer.hxx"
 #include "BRepTools.hxx"
@@ -68,18 +68,26 @@
 #include "IGESToBRep_Reader.hxx"
 #include "Interface_Static.hxx"
 #include "GeomAPI_ExtremaCurveCurve.hxx"
-#include "Standard_ErrorHandler.hxx"
+//#include "Standard_ErrorHandler.hxx"
 #include "Standard_Failure.hxx"
 #include "ShapeUpgrade_ShellSewing.hxx"
 #include "ShapeFix_Shape.hxx"
 #include "ShapeFix_Wireframe.hxx"
-#include "BRepMesh.hxx"
+#include <Standard_Version.hxx>
+#if (OCC_VERSION_MAJOR << 16 | OCC_VERSION_MINOR << 8 | OCC_VERSION_MAINTENANCE) <= 0x060702
+// porting to OCCT6.7.3
+  #include "BRepMesh.hxx"
+#endif
 #include "BRepMesh_IncrementalMesh.hxx"
 #include "BRepBndLib.hxx"
 #include "Bnd_Box.hxx"
 #include "ShapeAnalysis.hxx"
 #include "ShapeBuild_ReShape.hxx"
 
+// -- Optimization: to use cached projector and classifier
+#include <NCollection_DataMap.hxx>
+class Handle_ShapeAnalysis_Surface;
+class BRepTopAdaptor_FClass2d;
 
 // Philippose - 29/01/2009
 // OpenCascade XDE Support
@@ -192,6 +200,9 @@ namespace netgen
    class OCCGeometry : public NetgenGeometry
    {
       Point<3> center;
+	  // -- Optimization: to use cached projector and classifier
+      mutable NCollection_DataMap<int, Handle_ShapeAnalysis_Surface> fprjmap;
+      mutable NCollection_DataMap<int, BRepTopAdaptor_FClass2d*> fclsmap;
 
    public:
       TopoDS_Shape shape;
@@ -243,9 +254,9 @@ namespace netgen
          vmap.Clear();
       }
 
+	  ~OCCGeometry(); // -- to free cached project and classifier
 
-     DLL_HEADER virtual void Save (string filename) const;
-
+      DLL_HEADER virtual void Save (string filename) const;
 
       DLL_HEADER void BuildFMap();
 
@@ -266,8 +277,13 @@ namespace netgen
       Point<3> Center()
       {  return center;}
 
-      void Project (int surfi, Point<3> & p) const;
+      // void Project (int surfi, Point<3> & p) const;
+	  bool Project(int surfi, Point<3> & p, double& u, double& v) const;
       bool FastProject (int surfi, Point<3> & ap, double& u, double& v) const;
+
+	  // -- Optimization: to use cached projector and classifier
+	  void GetFaceTools(int surfi, Handle(ShapeAnalysis_Surface)& proj,
+		                BRepTopAdaptor_FClass2d*& cls) const;
 
       OCCSurface GetSurface (int surfi)
       {
